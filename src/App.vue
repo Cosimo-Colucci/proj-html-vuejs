e330ea829f09a45590865a1a12644642e7c2b183
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-06-26 16:04:04 +0200

    First commit Created a working floor with style by scss

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+export default{
+    name: "App",
+
+    data() {
+        return {
+          
+        };
+    },
+
+    components: {
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+  @import './styles/general.scss';
+
+</style>
