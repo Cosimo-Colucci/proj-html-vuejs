3a9ce67290fad36c12d45d8d2798e2d95a0fd666
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-06-26 16:23:51 +0200

    Created Appfooter component

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,22 @@
+<script>
+export default{
+    name: "AppFooter",
+
+    data() {
+        return {
+          
+        };
+    },
+
+    components: {
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+
+</style>
