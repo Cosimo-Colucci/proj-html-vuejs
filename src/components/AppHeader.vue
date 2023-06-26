318fd1c08cd9f903813c6df0d2133ba12d26096d
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-06-26 16:16:10 +0200

    created AppHeader component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+export default{
+    name: "AppHeader",
+
+    data() {
+        return {
+          
+        };
+    },
+
+    components: {
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+
+</style>
