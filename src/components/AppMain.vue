964ca7dd6ba433c8bce42826640a40027b129a89
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-06-26 16:22:29 +0200

    Created AppMain Component

---
+++ src/components/AppMain.vue
@@ -0,0 +1,22 @@
+<script>
+export default{
+    name: "AppMain",
+
+    data() {
+        return {
+          
+        };
+    },
+
+    components: {
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+
+</style>
