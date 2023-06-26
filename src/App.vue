22f3ad1dbaa69e4c7202d82258c30e3beed11ab1
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-06-26 16:32:36 +0200

    Imported AppHeader, AppMain,  AppFooter as macro components into App

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
 <script>
+  import AppHeader from './components/AppHeader.vue';
+  import AppMain from './components/AppMain.vue';
+  import AppFooter from './components/AppFooter.vue';
+
 export default{
     name: "App",
 
@@ -9,11 +13,17 @@ export default{
     },
 
     components: {
+      AppHeader,
+      AppMain,
+      AppFooter,
     }
 }
 </script>
 
 <template>
+  <AppHeader/>
+  <AppMain/>
+  <AppFooter/>
 
 </template>
 
