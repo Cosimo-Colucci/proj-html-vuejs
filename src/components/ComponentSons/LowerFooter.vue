cd1976be338e147055a9289d6caa0fccf4309aae
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-06-27 13:05:37 +0200

    implement of icon social into lowerFooter

--- src/components/ComponentSons/LowerFooter.vue
+++ src/components/ComponentSons/LowerFooter.vue
@@ -1,7 +1,25 @@
 <template>
-    <div>
-        ProjHtmlVuejsLowerFooter'
+    <div class="lower_footer">
+        <div class="copyright_info">
+            copyright_info
+        </div>
 
+        <div class="social_info">
+            <ul>
+                <li>
+                    <a href=""><i class="fa-brands fa-facebook-f"></i></a>
+                </li>
+                <li>
+                    <a href=""><i class="fa-brands fa-twitter"></i></a>
+                </li>
+                <li>
+                    <a href=""><i class="fa-brands fa-youtube"></i></a>
+                </li>
+                <li>
+                    <a href=""><i class="fa-brands fa-instagram"></i></a>
+                </li>
+            </ul>
+        </div>
     </div>
     
 </template>
@@ -27,5 +45,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    div.lower_footer{
+        background-color: black;
+        color: #ebebeb;
+        display: flex;
+        justify-content: space-between;
+        padding: 2rem;
 
+        div.social_info{
+            ul{
+                list-style-type: none;
+                display: flex;
+
+                li{
+                    margin: 0 .5rem;
+                    
+
+                    a{
+                        color: white;
+                        padding: .5rem;
+                        border-radius: 50%;
+                        background-color: #5addcd;
+                    }
+                }
+                
+            }
+        }
+    }
 </style>
