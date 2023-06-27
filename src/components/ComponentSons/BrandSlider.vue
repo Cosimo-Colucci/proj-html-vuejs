669f6dcb6f889f87dca5b0a7805cc61bb44fe1a2
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-06-27 12:05:57 +0200

    static slider with brand img

--- src/components/ComponentSons/BrandSlider.vue
+++ src/components/ComponentSons/BrandSlider.vue
@@ -1,6 +1,11 @@
 <template>
-    <div>
-        ProjHtmlVuejsBrandSlider'
+    <div class="brand_logo">
+        <img src="../../../public/img/logo-big/client/client-logo-1-1.png" alt="Client logo">
+        <img src="../../../public/img/logo-big/client/client-logo-2.png" alt="Client logo">
+        <img src="../../../public/img/logo-big/client/client-logo-3.png" alt="Client logo">
+        <img src="../../../public/img/logo-big/client/client-logo-4.png" alt="Client logo">
+        <img src="../../../public/img/logo-big/client/client-logo-5.png" alt="Client logo">
+        <img src="../../../public/img/logo-big/client/client-logo-6.png" alt="Client logo">
     </div>
 </template>
 
@@ -25,5 +30,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+    div.brand_logo{
+        display: flex;
+        justify-content: space-around;
+        flex-wrap: wrap;
+        background-color: #ebebeb;
+    }
 </style>
