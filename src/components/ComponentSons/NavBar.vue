2f4526035779071057edc132f46ddc674db96766
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-06-27 11:19:50 +0200

    created static navbar with logo and unordered list

---
+++ src/components/ComponentSons/NavBar.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="navbar">
+        <div class="navbar_logo">
+            <img src="../../../public/img/logo-big/finance/finance_logo_1x.png" alt="">
+        </div>
+        <div class="navbar_list">
+            <ul>
+                <li>home</li>
+                <li>about</li>
+                <li>service</li>
+                <li>packages</li>
+                <li>insight</li>
+                <li>
+                    <button>
+                        contact
+                    </button>
+                </li>
+                <li>
+                    <i class="fa-solid fa-plus"></i>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NavBar',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+    div.navbar{
+        display: flex;
+        justify-content: space-between;
+        padding: 1.5rem;
+
+        div.navbar_list>ul{
+            display: flex;
+            list-style-type: none;
+            padding: 1rem;
+
+            li, button{
+                margin: 0 1rem;
+                text-transform: uppercase;
+            }
+        }
+    }
+</style>
