4fa9c31aa43c0ed4bf0727002ed540f0f71232c4
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-06-28 11:04:38 +0200

    section of cloud finance with graph

---
+++ src/components/ComponentSons/GraphMain.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="container ">
+        <div class="row">
+            <div class="col-6">
+                <h1 class="mb-4">
+                    Cloud Based Finance
+                </h1>
+                <span class="mb-4"></span>
+                <p class="mb-4">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non atque pariatur unde inventore molestiae quibusdam ducimus quasi at assumenda sed rerum, qui dolorem. Cumque dolorum eligendi, neque illum minus error.
+                </p>
+                <p class="mb-4">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non atque pariatur unde inventore molestiae quibusdam ducimus quasi at assumenda sed rerum, qui dolorem. Cumque dolorum eligendi, neque illum minus error.
+                </p>
+                <button class="mb-4">
+                    Learn More
+                </button>
+            </div>
+            <div class="col-6">
+                <img src="../../../public/img/graph/graph-800x610.jpg" alt="">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'GraphMain',
+
+        data() {
+            return {
+                
+            };
+        },
+
+        mounted() {
+            
+        },
+
+        methods: {
+            
+        },
+    };
+</script>
+
+<style lang="scss" scoped>
+    h1{
+        margin-top: 6.5rem;
+    }
+    span{
+        display: block;
+        width: 80px;
+        height: 2px;
+        background-color: black;
+    }
+</style>
