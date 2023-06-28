53fc5eba06e168e49d09cc0067d1abb8fff3ecf1
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-06-28 17:58:17 +0200

    Created Talk Component with first layout

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,6 +3,7 @@
     import GraphMain from './ComponentSons/GraphMain.vue';
     import ServiseComponent from './ComponentSons/ServiseComponent.vue';
     import Communities from './ComponentSons/Communities.vue';
+    import Talk from './ComponentSons/Talk.vue';
 
     export default{
         name: "AppMain",
@@ -18,6 +19,7 @@
             GraphMain,
             ServiseComponent,
             Communities,
+            Talk,
         }
 }
 </script>
@@ -27,6 +29,7 @@
     <ServiseComponent/>
     <Communities/>
     <ClientFeedback/>
+    <Talk/>
 </template>
 
 <style lang="scss" scoped>
---
+++ src/components/ComponentSons/Talk.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="body">
+        <div class="container my_container text-center">
+            <h1>
+                Ready to Talk?
+            </h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, quod ipsum? Aut repellat fugiat saepe suscipit, architecto dolorem adipisci cumque voluptatum laborum, debitis commodi eligendi tempore consequuntur animi quaerat doloribus?
+            </p>
+            <button>
+                let's talk
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Talk',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+    div.body{
+        width: 100%;
+        background: url(../../../public/img/ready_to_talk_cta_bg.jpg);
+        object-fit: fill;
+    }
+
+    div.my_container{
+        color: white;
+        padding: 5rem;
+    }
+
+    button{
+        color: white;
+        background-color: #5ADDCD;
+    }
+</style>
